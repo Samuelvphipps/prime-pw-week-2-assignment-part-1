d9863521e38131163053c0f33ffb960af1688844
Sam Phipps <samuel.v.phipps@gmail.com>
2022-08-23 11:28:17 -0500

    final edits

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,9 +2,11 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 const firstName='Sam';
 
+console.log(firstName);
+
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 const lastName='Phipps';
-
+console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 const fullName = `${firstName} ${lastName}`
@@ -14,7 +16,7 @@ console.log(fullName);
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 const luckyNumber = 5
-
+console.log(luckyNumber);
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
@@ -23,21 +25,23 @@ console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true;
+console.log(adventurous);
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food= 'pizza';
-
+console.log(food);
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets=1
-
+console.log(pets);
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets=5
-
+console.log(friendsPets);
 // 11 - Add two pets to your `pets` variable
 pets+=2;
 console.log(pets);
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets=3
+console.log(allowedPets);
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
