451f767cb1168a417a34155af667b87356829450
Sam Phipps <samuel.v.phipps@gmail.com>
2022-08-23 10:43:57 -0500

    Finished required scripting

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -22,28 +22,41 @@ console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 // Refer back to the videos if you need help with this one.
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
+let food= 'pizza';
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+let pets=1
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+let friendsPets=2
 
 // 11 - Add two pets to your `pets` variable
+pets+=2;
+console.log(pets);
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+const allowedPets=3
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if(adventurous){console.log('Adventures are great!');}
+else{console.log('How about we stay home?')};
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if(luckyNumber==2 && adventurous==true){console.log('Roll the dice!');}
+else{console.log('Better luck next time!');}
+// I added the else so I could better check my work in the console
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
+if(pets<allowedPets){console.log('I can have more pets!');}
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
+else if(pets==allowedPets){console.log('I have enough pets!');}
 // and console logs "Oh no, I have too many pets!"
+else {console.log('Oh no, I have too many pets!');}
 // if the value of `pets` is greater than the value of `allowedPets`.
 
 // STRETCH GOALS:
