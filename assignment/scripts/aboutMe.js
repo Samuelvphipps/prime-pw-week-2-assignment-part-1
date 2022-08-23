3bb0d0169dffb7cafb259d6de248f4ec84a33f56
Sam Phipps <samuel.v.phipps@gmail.com>
2022-08-23 11:25:10 -0500

    Finished all scripts

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -13,7 +13,7 @@ const fullName = `${firstName} ${lastName}`
 console.log(fullName);
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-const luckyNumber = 17
+const luckyNumber = 5
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
@@ -79,6 +79,14 @@ console.log(mostPets);
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch(luckyNumber){
+    case 1: console.log('First is the worst'); break;
+    case 2: console.log('Second is the best'); break;
+    case 3: console.log('Third is the one with the polka dot dress'); break;
+default: console.log('Luck is what happens when preparation meets opportunity'); break;
+};
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
-
+adventurous? console.log('Adventures are great!'):
+console.log('How about we stay home?');
